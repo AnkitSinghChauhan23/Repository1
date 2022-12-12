fd192d71ea905fb23533482452a8ed3dfe15b8e7
XYZ <XYZ@DESKTOP-I03194S>
2022-12-12 12:32:19 +0530

    System.out.println("Hello world- Local Repo101");

--- TestDynamicWebProject1/src/main/java/testPackage/HelloWorld.java
+++ TestDynamicWebProject1/src/main/java/testPackage/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world- Eclipse project connected");
+		System.out.println("Hello world- Local Repo101");
 	}
 }
